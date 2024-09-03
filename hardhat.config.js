ceba9a3e8d57f62ea67df2ff1e63df7000d371f3
Om Pandey <mynameisompandey007@gmail.com>
2024-09-03 20:19:48 +0530

    contract deployed Locally

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 require('@nomicfoundation/hardhat-ignition')
 require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks:{
+    localhost:{
+      url:"http://127.0.0.1:8545"
+    }
+  }
 };
+
+
---
+++ ignition/modules/simpleStorage.js
@@ -0,0 +1,7 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+module.exports = buildModule("SimpleStorageToken", (m) => {
+  const token = m.contract("simpleStorage");
+
+  return { token };
+});
